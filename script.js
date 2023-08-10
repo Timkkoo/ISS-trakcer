bfe8c8002a18ec09906da7232d7f178b967aaf58
Tomasz Łysoń <kemotlyson@gmail.com>
2023-08-10 20:00:17 +0200

    Dodano ,,szkielet'' strony, przygotowanie pod wykorzystanie danych, JS - dodano zegar oraz podłaczono API

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
-//zmiana koloru ikony strony w zależności od motywu przeglądarki 
-var element = document.querySelector("link[rel='icon']");
+//change of icon color depending on the browser theme
+let iconElement = document.querySelector("link[rel='icon']");
+const timeParagraph = document.querySelector('.time');
+
+const apiURL = 'https://api.wheretheiss.at/v1/satellites/25544'; //25544 stands for ISS
+
+async function getData() {
+  try {
+    const response = await fetch(apiURL);
+    if (response.ok) {
+      const data = await response.json();
+    } else {
+      response.text().then((text) => {
+        throw new Error(text);
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 const darkModeSet = (event) => {
-   if (event.matches) {
-        element.setAttribute('href', 'images/iss-dark-mode-icon.png');
-        console.log('dark mode');
-    } else  {
-        element.setAttribute('href', 'images/iss-light-mode-icon.png');
-        console.log('light mode');
-    }
+  if (event.matches) {
+    IntersectionObserverlement.setAttribute(
+      'href',
+      'images/iss-dark-mode-icon.png'
+    );
+    console.log('dark mode');
+  } else {
+    iconElement.setAttribute('href', 'images/iss-light-mode-icon.png');
+    console.log('light mode');
+  }
 };
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', darkModeSet);
+window
+  .matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', darkModeSet);
+
+//clock
+setInterval(() => {
+  let time = new Date().toLocaleTimeString();
+  timeParagraph.textContent = time;
+}, 1);
+
+setInterval(getData, 1000);
+
