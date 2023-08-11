336a11fc02b5174d76c04b53c97dd50098bb63e4
Tomasz Łysoń <kemotlyson@gmail.com>
2023-08-11 16:26:42 +0200

    Dodano w tabeli wartości które będą ulegały zmianie, JS - dodano zmiane ikony oraz tesktu w zależności od położenia stacji

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-//change of icon color depending on the browser theme
+//variables for change of icon color depending on the browser theme
 let iconElement = document.querySelector("link[rel='icon']");
 const timeParagraph = document.querySelector('.time');
 
+// variables for updatingData
+const issVisibilityParagraph = document.querySelector('#iss-vis');
+const sunIcon = document.querySelector('.sun-icon');
+const moonIcon = document.querySelector('.moon-icon');
+
 const apiURL = 'https://api.wheretheiss.at/v1/satellites/25544'; //25544 stands for ISS
 
-async function getData() {
+async function getData() { //fetching data from API
   try {
     const response = await fetch(apiURL);
     if (response.ok) {
       const data = await response.json();
+      updateData(data);
+      console.log(data);
     } else {
       response.text().then((text) => {
         throw new Error(text);
@@ -19,7 +26,19 @@ async function getData() {
   }
 }
 
-const darkModeSet = (event) => {
+const updateData = (data) => {   //updating data on site 
+  let issVisibility = data.visibility;
+  const issVisibilityText = 'ISS is in '
+  if(issVisibility == 'daylight') {
+    issVisibilityParagraph.textContent = issVisibilityText + issVisibility;
+    sunIcon.style.display = 'block';
+  } else if (issVisibility == 'eclipsed') {
+    issVisibilityParagraph.textContent = issVisibilityText + `Earth's shadow`;
+    moonIcon.style.display = 'block';
+  }
+};
+
+const darkModeSet = (event) => {  //setting the color of the icon depending on the browser theme
   if (event.matches) {
     IntersectionObserverlement.setAttribute(
       'href',
@@ -42,5 +61,5 @@ setInterval(() => {
   timeParagraph.textContent = time;
 }, 1);
 
-setInterval(getData, 1000);
+setInterval(getData, 1000);  //refreshing the position every second
 
