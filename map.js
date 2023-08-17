f9dd5c0bd93acaca900003933baa2b7753e6cb90
Tomasz Łysoń <kemotlyson@gmail.com>
2023-08-17 19:28:09 +0200

    Ikona załadowana poprawnie

--- map.js
+++ map.js
@@ -1,13 +1,4 @@
-let map = L.map('issMap').setView([0, 0], 2); //
-
-var issIcon = L.Icon.extend({
-  options: {
-    // z dokumentacji
-    iconURL: 'images/iss-marker.png',
-    iconSize: [15, 15],
-    iconAnchor: [25, 16]
-  }}
-);
+let map = L.map('issMap').setView([0, 0], 4); //
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
@@ -15,19 +6,30 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-// const updatePostion = (latitude, longitude) => {
-//   // unikanie hoistingu
- 
-// };
+var iconOptions = {
+  iconUrl: 'images/iss-marker.png',
+  iconSize: [100, 60],
+  iconAnchor: [25, 16]
+}
 
-map.addLayer(L.marker([0, 0], { icon: issIcon }).addTo(map));
+var issIcon = L.icon(iconOptions);
+
+var issMarkerOptions = {
+  icon: issIcon
+}
+
+
+
+const updatePostion = (latitude, longitude) => {
+  L.marker([latitude, longitude], issMarkerOptions).addTo(map);
+};
+
 recieveData();
 
 async function recieveData() {
   const latitudeRecieved = sessionStorage.getItem('latitudeFromAPI'); //szerokość
   const longitudeRecieved = sessionStorage.getItem('longitudeFromAPI'); //długość
-  // updatePostion(latitudeRecieved, longitudeRecieved); 
-  
+  updatePostion(latitudeRecieved, longitudeRecieved); 
 }
 
 setInterval(recieveData, 1000);
