4cb51e4338ffb6d5df712623286c537801293874
Tomasz Łysoń <kemotlyson@gmail.com>
2023-09-06 14:41:41 +0200

    Poprawiono link API

--- issPeopleAPI.js
+++ issPeopleAPI.js
@@ -1,6 +1,6 @@
 //this script is responsible for fetching API and updating data about people in ISS
 
-const peopleApiURL = 'http://api.open-notify.org/astros.json';
+const peopleApiURL = 'https://api.open-notify.org/astros.json';
 
 const peopleTable = document.querySelector('.people-data');
 
